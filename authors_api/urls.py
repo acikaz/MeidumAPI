48cef467728eca8cb3836e505b0fe0f0cf19a5f9
azec <azec@planetsoft.ba>
2024-02-24 11:17:50 +0100

    feat: added custom user modesl on the admin portal, added new redoc feat for documentig apis

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -1,12 +1,34 @@
 
+#from dj_rest_auth.views import PasswordResetConfirmView
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+#from core_apps.users.views import CustomUserDetailsView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors Haven API",
+        default_version="v1",
+        description="API endpoints for Authors Haven API Course",
+        contact=openapi.Contact(email="api.imperfect@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
-#commment
+admin.site.site_header = "Acika admin users"
 
-# comment 2
+admin.site.site_title = "Acika api admin portal"
+
+admin.site.index_title = "Welcome to Acikas site"
+
