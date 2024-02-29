770cca77d6e795792887b7a6606a167fbbf7c9a5
Aleksandar Zec <52027726+acikaz@users.noreply.github.com>
2024-02-29 20:34:15 +0100

    feat: added prfile paggination and views

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
         PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
+    path("api/v1/profiles/", include("core_apps.profiles.urls")),
 ]
 
 admin.site.site_header = "Acika admin users"
